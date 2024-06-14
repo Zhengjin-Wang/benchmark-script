bb874c29d7fdd1909935b70db7950b74e17f6e2a
Zhengjin-Wang <1054129023@qq.com>
2024-06-14 16:52:05 +0800

    concurrency rate

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # benchmark-script
+
+## 并发效率
+通过`ps -efL | grep ${pid}` 查看进程中的线程。
+
+输入: 进程pid
+
+输出: 进程的线程列表
---
+++ concurrency_rate.py
@@ -0,0 +1,22 @@
+import argparse
+import subprocess
+import csv
+
+parser = argparse.ArgumentParser(description='Collect concurrency rate.')
+parser.add_argument('--pid',default="", type=str, help='')
+args = parser.parse_args()
+pid = args.pid
+
+ps = subprocess.Popen(('ps', '-efL'), stdout=subprocess.PIPE, text=True)
+output = subprocess.check_output(('grep', pid), stdin=ps.stdout, text=True)
+ps.wait()
+
+rows = output.split('\n')
+rows = rows[:-3]
+with open(f'./concurrency_rate.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    for row in rows:
+        writer.writerow([row])
+    writer.writerow([f'Total thread(s): {len(rows)}'])
+
+thread_num = len(rows)
